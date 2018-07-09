750b3523c9570dd0d91d10ee310a2e32b66f7f42
Diego Gay Saez <diego.gay@gmail.com>
2018-07-09 22:26:04 +0200

    Template literals

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Diego',
+  surname: 'Gay Saez',
+  address: 'C/ Calandria, 13',
+  phone: '666666665'
+}
+
+const toPrint = `
+  Nombre: ${user.name};
+  Apellidos: ${user.surname};
+  Dirección: ${user.address};
+  Teléfono: ${user.phone};
+`;
+
+const {name, surname, address, phone} = user;
+const toPrint2 = `
+Nombre: ${name};
+Apellidos: ${surname};
+Dirección: ${address};
+Teléfono: ${phone};
+`;
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{toPrint2}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
