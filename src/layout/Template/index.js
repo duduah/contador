75fbc64385889be0f54bad0d633a436fa3546d45
Diego Gay Saez <diego.gay@gmail.com>
2018-07-14 21:33:04 +0200

    Using styled-components and normalize to remove margins set by the browser

--- src/layout/Template/index.js
+++ src/layout/Template/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { injectGlobal } from 'styled-components';
+import { normalize } from 'polished';
 
 import Header from '../Header';
 import Body from '../Body';
+
+// eslint-disable-next-line
+injectGlobal`
+  ${normalize()};
+`;
 
 const Template = ({ counterValue, logo }) => (
   <div>
