19700279bc7bfca524b633123ede80acd97e5142
Diego Gay Saez <diego.gay@gmail.com>
2018-07-14 13:22:24 +0200

    Dependencies installed. Eslint configured to admin JSX in js files

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function sumParams(...params) {
-  return params.reduce((acc, curr) => acc + curr, 0)
+  return params.reduce((acc, curr) => acc + curr, 0);
 }
 
 const myObj = {
   d: 'fuu',
   c: 'foo',
   b: 'bar',
-  a: 'baz'
+  a: 'baz',
 };
 
-const { a, b, ...rest} = myObj
+const { a, b, ...rest } = myObj;
 
 const newObj = {
   ...rest,
-  d: 'fuh'
+  d: 'fuh',
 };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Diego GS</h1>
-        </header>
-        <p className="App-intro">
-        <p>
-          <b>a:</b>
-          {a}
-        </p>
-        <p>
-          <b>b:</b>
-          {b}
-        </p>
-        <p>
-          <b>rest:</b>
-          {JSON.stringify(rest)}
-        </p>
-        <p>
-          <b>newObj:</b>
-          {JSON.stringify(newObj)}
-        </p>
-        <p>
-          <b>sumParams: </b>
-          {sumParams(1, 2, 3, 4)}
-        </p>
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">
+        Diego GS
+      </h1>
+    </header>
+    <p className="App-intro">
+      <p>
+        <b>
+          a:
+        </b>
+        {a}
+      </p>
+      <p>
+        <b>
+          b:
+        </b>
+        {b}
+      </p>
+      <p>
+        <b>
+          rest:
+        </b>
+        {JSON.stringify(rest)}
+      </p>
+      <p>
+        <b>
+          newObj:
+        </b>
+        {JSON.stringify(newObj)}
+      </p>
+      <p>
+        <b>
+          sumParams:
+        </b>
+        {sumParams(1, 2, 3, 4)}
+      </p>
+    </p>
+  </div>
+);
+
 
 export default App;
