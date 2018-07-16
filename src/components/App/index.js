30eb9ea50fe58643ac7cd32144dd59ccd767f93c
Diego Gay Saez <diego.gay@gmail.com>
2018-07-16 21:53:24 +0200

    Destructurin value. Here you can change the value in the browser and the component render it

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,17 +5,16 @@ import CounterValue from '../CounterValue';
 import OperationButtons from '../OperationButtons';
 import ResetButtons from '../ResetButtons';
 
-const value = 10;
-
 class App extends Component {
   state = {
     value: 6,
   };
 
   render() {
+    const { value } = this.state;
     return (
       <Template logo="https://keepcoding.io/es/wp-content/uploads/sites/4/2015/05/logo-keepcoding-web.png">
-        <CounterValue value={this.state.value} />
+        <CounterValue value={value} />
         {value > -10 && value < 10 ? <OperationButtons /> : <ResetButtons />}
       </Template>
     );
