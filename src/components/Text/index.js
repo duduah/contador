e63ed5db1a34c423cf0e4a80eb708ab97dbc3bab
Diego Gay Saez <diego.gay@gmail.com>
2018-07-16 19:56:36 +0200

    Mapping styles using styled-map component (installed like this: yarn add styled-map). Replacing nested ternary expresion with mapped styles. Remove Text style extension from ResetButtons.

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,19 +1,26 @@
 import styled from 'styled-components';
+import styledMap from 'styled-map';
 import PropTypes from 'prop-types';
 
 const StyledText = styled.p`
   margin: 0;
   font-size: 22px;
   text-align: ${({ center }) => (center ? 'center' : 'left')};
-  color: ${({ color }) => (color == 'white'
-    ? '#fff'
-    : color == 'danger'
-      ? '#ff0000'
-      : color == 'primary'
-        ? '#0066cc'
-        : color == 'success'
-          ? 'green'
-          : '#333')};
+  color: ${styledMap('color', {
+    white: '#fff',
+    danger: '#ff0000',
+    primary: '#0066cc',
+    success: 'green',
+    default: '#333',
+  })};
+  background: ${styledMap('background', {
+    white: '#fff',
+    danger: '#ff0000',
+    primary: '#0066cc',
+    success: 'green',
+    black: '#333',
+    default: 'transparent',
+  })};
 `;
 
 StyledText.defaultProps = {
