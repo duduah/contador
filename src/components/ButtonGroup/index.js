29c35a07f9928aae12c9fdca41973a6fa202ddf1
Diego Gay Saez <diego.gay@gmail.com>
2018-07-16 20:34:38 +0200

    Extending Flex Style for ButtonGroup

--- src/components/ButtonGroup/index.js
+++ src/components/ButtonGroup/index.js
@@ -3,8 +3,12 @@ import PropTypes from 'prop-types';
 
 import Flex from '../Flex';
 
+const StyledFlex = Flex.extend`
+  border: 1px solid red;
+`;
+
 const ButtonGroup = ({ center, ...props }) => (
-  <Flex justifyContent={center ? 'center' : 'space-between'} {...props} />
+  <StyledFlex justifyContent={center ? 'center' : 'space-between'} {...props} />
 );
 
 ButtonGroup.defaultProps = {
