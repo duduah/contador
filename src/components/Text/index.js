9797d172672b791ec4fbfc644379d4a04054fe1e
Diego Gay Saez <diego.gay@gmail.com>
2018-07-16 19:46:21 +0200

    DON'T TRY THIS AT HOME! Nested ternary expressions are not recommended.

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,10 +1,29 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledText = styled.p`
   margin: 0;
   font-size: 22px;
   text-align: ${({ center }) => (center ? 'center' : 'left')};
-  color: ${({ color }) => color || '#333'};
+  color: ${({ color }) => (color == 'white'
+    ? '#fff'
+    : color == 'danger'
+      ? '#ff0000'
+      : color == 'primary'
+        ? '#0066cc'
+        : color == 'success'
+          ? 'green'
+          : '#333')};
 `;
+
+StyledText.defaultProps = {
+  color: 'black',
+  background: 'transparent',
+};
+
+StyledText.propTypes = {
+  color: PropTypes.oneOf(['white', 'danger', 'primary', 'success', 'black']),
+  background: PropTypes.oneOf(['white', 'danger', 'primary', 'success', 'black', 'transparent']),
+};
 
 export default StyledText;
