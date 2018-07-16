72990fbfff1c039114086a404d58287beba4c752
Diego Gay Saez <diego.gay@gmail.com>
2018-07-16 20:36:44 +0200

    Undo last changes.

--- src/components/ButtonGroup/index.js
+++ src/components/ButtonGroup/index.js
@@ -3,12 +3,8 @@ import PropTypes from 'prop-types';
 
 import Flex from '../Flex';
 
-const StyledFlex = Flex.extend`
-  border: 1px solid red;
-`;
-
 const ButtonGroup = ({ center, ...props }) => (
-  <StyledFlex justifyContent={center ? 'center' : 'space-between'} {...props} />
+  <Flex justifyContent={center ? 'center' : 'space-between'} {...props} />
 );
 
 ButtonGroup.defaultProps = {
